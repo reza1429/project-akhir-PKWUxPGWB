f41eae713a1ffda11c8e73d11360fccdd806692d
duwikk <fmochammaddwi@gmail.com>
2022-11-11 09:30:59 +0700

    k

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -7,17 +7,16 @@
         <!-- Outer Row -->
         <div class="row justify-content-center">
 
-            <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="col-xl-12 col-lg-12 col-md-9">
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
                             
-                            <div class="col-lg-6 bg-gradient-success">
+                            <div class="col-lg-6 bg-success">
                                 <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                    <div class="text-center">   
                                     </div>
                                     @if ($message = Session::get('success'))
                                     <div class="alert alert-success">
